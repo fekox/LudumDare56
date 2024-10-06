9b6470dcea6964ec2c320ade5bec0e2ed46c59b4
fekox <facusantos123@gmail.com>
2024-10-06 20:11:00 -0300

    Implement a check for the spawner on the rooms

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -25,6 +27,20 @@ public class GameManager : MonoBehaviour
 
     [SerializeField] private CanvasUI canvasUI;
 
+    [Header("Spawn points")]
+    [SerializeField] private List<GameObject> rooms;
+
+    [SerializeField] private GameObject door1;
+    [SerializeField] private GameObject door2;
+    [SerializeField] private GameObject door3;
+    [SerializeField] private GameObject door4;
+    [SerializeField] private GameObject door5;
+
+    private bool isLivingActive;
+    private bool isBathroomActive;
+    private bool isKitchenActive;
+
+
     private void OnEnable()
     {
         Cursor.lockState = CursorLockMode.Locked;
@@ -37,14 +53,14 @@ public class GameManager : MonoBehaviour
 
         playerHealth.TakeDamageLogic();
 
-        for (int i = 0; i < pickUpWeapon.Count; i++) 
+        for (int i = 0; i < pickUpWeapon.Count; i++)
         {
             if (pickUpWeapon[i] == null)
             {
                 pickUpWeapon.Remove(pickUpWeapon[i]);
             }
 
-            else 
+            else
             {
                 pickUpWeapon[i].PickUpLogic();
             }
@@ -56,5 +72,60 @@ public class GameManager : MonoBehaviour
         playerCrouch.CrouchLogic();
 
         canvasUI.UpdatePointText();
+
+        CheckKitchen();
+        CheckBathroom();
+        CheckLiving();
+    }
+    
+    private void ActiveKitchen() 
+    {
+        rooms[1].SetActive(true);
+        isKitchenActive = true;
+    }
+
+    private void CheckKitchen() 
+    {
+        if (!isKitchenActive) 
+        {
+            if (door3.IsDestroyed() || door4.IsDestroyed() || door5.IsDestroyed())
+            {
+                ActiveKitchen();
+            }
+        }
+    }
+
+    private void ActiveBathroom() 
+    {
+        rooms[2].SetActive(true);
+        isBathroomActive = true;
+    }
+
+    private void CheckBathroom() 
+    {
+        if (!isBathroomActive)
+        {
+            if (door2.IsDestroyed() || door5.IsDestroyed())
+            {
+                ActiveBathroom();
+            }
+        }
+    }
+
+    private void ActiveLivingroom() 
+    {
+        rooms[3].SetActive(true);
+        isLivingActive = true;
+    }
+
+    private void CheckLiving()
+    {
+        if (!isLivingActive)
+        {
+            if (door1.IsDestroyed() || door2.IsDestroyed() || door3.IsDestroyed())
+            {
+                ActiveLivingroom();
+            }
+        }
     }
 }
