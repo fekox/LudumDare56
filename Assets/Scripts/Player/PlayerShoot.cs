ae567acd493cfaae63dbcfe01752903b14fdd9ea
AlejoSalvador <alelucmdp@hotmail.com>
2024-10-06 16:24:43 -0300

    SpawnerRelatedStuff

--- Assets/Scripts/Player/PlayerShoot.cs
+++ Assets/Scripts/Player/PlayerShoot.cs
@@ -84,9 +84,16 @@ public class PlayerShoot : MonoBehaviour
             
             EnemyAI enemy = hit.transform.GetComponent<EnemyAI>();
 
+            Spawner spawner = hit.transform.GetComponent<Spawner>();
+
             if (objectGO != null) 
             {
                 objectGO.TakeDamage(weapon.GetDamage());
+            }
+
+            if (spawner != null)
+            {
+                spawner.TakeDamage(weapon.GetDamage());
             }
 
             if (enemy != null)
